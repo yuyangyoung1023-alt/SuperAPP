c87b8c41a5ead3e4f10d8d23c85e02e00b017503
yuyangyoung1023-alt <yu.yang.young1023@gmail.com>
2026-08-24 17:19:40 +0800

    Add sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='icar-claw-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{let copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html'))))});

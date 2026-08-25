bd8d3fcb24a386a369c4247177cccc545f89d949
yuyangyoung1023-alt <yu.yang.young1023@gmail.com>
2026-08-25 12:19:59 +0800

    fix: keep demo dialogs pinned to the car display

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='icar-claw-v5';
+const CACHE='icar-claw-v6';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./assets/icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
